db9adb83b173526ee0244a6e306ddb97738b34e7
Sydney Flak <sflak@Sydneys-MacBook-Pro.local>
2017-12-31 11:42:11 -0800

    Ingredients can be added using the more button

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const buildControl = (props) => (
-  <div>
-    <div>{props.label}</div>
-    <button>Less</button>
-    <button>More</button>
+import classes from './BuildControl.css'
 
+const buildControl = (props) => (
+  <div className={classes.BuildControl}>
+    <div className={classes.Label}>{props.label}</div>
+    <button className={classes.Less}>Less</button>
+    <button className={classes.More} onClick={props.added}>More</button>
   </div>
 );
 
