592297abd0238a516f437985369d070e6dc77a2f
Sydney Flak <sflak@porter-kresge-apts-58-70.resnet.ucsc.edu>
2018-01-08 09:45:06 -0800

    convert layout to class component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
@@ -6,15 +6,33 @@ import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 import classes from './Layout.css';
 
 
-const layout = (props) => (
-  <Aux>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Aux>
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  }
+  sideDrawerCloseHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+  sideDrawerToggleHandler = () => {
+    this.setState((prevState) => {
+      return {showSideDrawer: !prevState.showSideDrawer}
+    });
+  }
 
-);
+  render () {
+    return (
+      <Aux>
+        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+        <main className={classes.Content}>
+          {this.props.children}
+        </main>
+      </Aux>
+    )
+  }
+}
 
-export default layout;
+
+
+
+export default Layout;
